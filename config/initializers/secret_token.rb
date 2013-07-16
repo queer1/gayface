c337dd1c783cf242474f03ef8c139852bffd9996
SamanthaRadocchia <samantha.radocchia@gmail.com>
2013-07-15 22:38:28 -0400

    creates refinery cms app with photo_gallery gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gayface::Application.config.secret_token = 'd12528265e54fa55d3bed36cfea915c8a4d84aeafa72f79d933e6c88755c984cb9ffc123c0e1853a867f8585928c8662f663d9cdf6b9021a2d330f38a53d207a'
