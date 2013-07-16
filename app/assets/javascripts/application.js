9aa5752fa576ef2e5b231f2148adbcefc1930735
SamanthaRadocchia <samantha.radocchia@gmail.com>
2013-07-16 03:32:59 -0400

    adds jquery requirements to application.js file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,23 @@
 //= require_tree .
 // Loads all Bootstrap javascripts
 //= require bootstrap
+//= require jquery.ui.all
+//= require carousel.js
+//= require custom.js
+//= require excanvas.js
+//= require flexslider.js
+//= require jquery-1.9.1..min.js
+//= require jquery.easing-1.3.js
+//= require jquery.cslider.js
+//= require jquery.fancybox.js
+//= require jquery.flot.js
+//= require jquery.flot.pie.min.js
+//= require jquery.flot.resize.min.js
+//= require jquery.flot.stack.js
+//= require jquery.gmap.min.js
+//= require jquery.isotope.min.js
+//= require modernizr.js
+//= require retina.js
+//= require slider.js
+//= require twitter.js
+
