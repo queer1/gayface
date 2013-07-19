4b59f7f7b7b29041c06e2bf8f8b6cfdc8ad0f9d9
SamanthaRadocchia <samantha.radocchia@gmail.com>
2013-07-19 01:12:19 -0400

    messing around with design and flexslider, nico, and other javascript stuff ugh

--- config/routes.rb
+++ config/routes.rb
@@ -61,7 +61,7 @@ Gayface::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  # root :to => 'gayface_users#index'
 
   # See how all your routes lay out with "rake routes"
 
